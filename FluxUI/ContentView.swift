d09e3e02ba0128f126a47b3be175748864a1a9cb
Frizlab <fload@me.com>
2020-08-01 08:19:39 +0200

    Initial Commit

---
+++ FluxUI/ContentView.swift
@@ -0,0 +1,25 @@
+/*
+ * ContentView.swift
+ * FluxUI
+ *
+ * Created by François Lamboley on 01/08/2020.
+ */
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+	var body: some View {
+		Text("Hello, world!").padding()
+	}
+}
+
+
+/* *************** */
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ FluxUI/FluxUIApp.swift
@@ -0,0 +1,27 @@
+/*
+ * FluxUIApp.swift
+ * FluxUI
+ *
+ * Created by François Lamboley on 01/08/2020.
+ */
+
+import SwiftUI
+
+
+
+@main
+struct FluxUIApp: App {
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+	
+}
+
+struct FluxUIApp_Previews: PreviewProvider {
+	static var previews: some View {
+		/*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+	}
+}
