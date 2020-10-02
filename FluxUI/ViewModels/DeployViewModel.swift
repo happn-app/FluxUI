0abf7ae11c4185f7344cffc00b071c90c3c52e7b
Frizlab <fload@me.com>
2020-10-02 12:11:49 +0200

    Implement fluxctl-less deploy view model

--- FluxUI/ViewModels/DeployViewModel.swift
+++ FluxUI/ViewModels/DeployViewModel.swift
@@ -16,6 +16,8 @@ limitations under the License. */
 import Combine
 import Foundation
 
+import Alamofire
+
 
 
 class DeployViewModel : ObservableObject {
@@ -56,37 +58,32 @@ class DeployViewModel : ObservableObject {
 		}
 		let workloadID = self.workloadID
 		
-		loadQueue.async{
-			do {
-				guard let executableURL = Bundle(for: type(of: self)).url(forAuxiliaryExecutable: "fluxctl") else {
-					throw SimpleError(message: "Internal error: Cannot find fluxctl, which is annoying because it should be built-in FluxUI!")
-				}
-				
-				let p = Process()
-				p.executableURL = executableURL
-				p.arguments = ["--url", fluxSettings.url.absoluteString, "release", "--watch", "--namespace", fluxSettings.namespace, "--workload", workloadID, "--update-image", containerID]
-				
-				let pipe = Pipe()
-				p.standardInput = nil
-				p.standardOutput = pipe
-				p.standardError = pipe
-				
-				p.launch()
-				p.waitUntilExit()
-				
-				let output = (try? pipe.fileHandleForReading.readToEnd()).flatMap{ String(data: $0, encoding: .utf8) }
-				guard p.terminationStatus == 0 else {
-					throw SimpleError(message: "fluxctl exited with a error code \(p.terminationStatus). fluxctl output: \(output ?? "<no output, or invalid utf8>")")
-				}
-				DispatchQueue.main.sync{
-					self.deployStatus = .deployed(.success(()))
-				}
-			} catch {
-				DispatchQueue.main.sync{
-					self.deployStatus = .deployed(.failure(error))
-				}
+		updateManifestAndWaitForSuccess(fluxURL: fluxSettings.url, handler: { result in
+			switch result {
+				case .failure(let e):
+					self.failDeployment(e)
+					
+				case .success:
+					let manifestUpdateParams = UpdateManifestData(
+						type: .image,
+						cause: UpdateManifestData.Cause(Message: "", User: "FluxUI <flux-ui@happn.com>"),
+						spec: UpdateManifestData.Spec(ServiceSpec: [workloadID], ImageSpec: containerID, Kind: "execute", Force: false)
+					)
+					self.updateManifestAndWaitForSuccess(manifestUpdateParams, fluxURL: fluxSettings.url){ result in
+						switch result {
+							case .failure(let e):
+								self.failDeployment(e)
+								
+							case .success:
+								self.updateManifestAndWaitForSuccess(fluxURL: fluxSettings.url){ result in
+									DispatchQueue.main.sync{
+										self.deployStatus = .deployed(result)
+									}
+								}
+						}
+					}
 			}
-		}
+		})
 	}
 	
 	/** Resets the deployment status to .idle if the status was .deployed. */
@@ -97,5 +94,68 @@ class DeployViewModel : ObservableObject {
 	}
 	
 	private let loadQueue = DispatchQueue(label: Constants.appBundleId + ".deploy-queue")
+	
+	private struct UpdateManifestData : Encodable {
+		enum UpdateType : String, Encodable {
+			case sync
+			case image
+		}
+		struct Cause : Encodable {
+			/* Too lazy to do the whole encoding key name dance rn (regarding
+			 * capitalization). */
+			var Message: String = ""
+			var User: String = ""
+		}
+		struct Spec : Encodable {
+			var ServiceSpec: [String]?
+			var ImageSpec: String?
+			var Kind: String?
+			var Force: Bool?
+			/* There’s also "Excludes":null in the sent object but we try without
+			 * because I don’t know its type and we’d need to implement the encode
+			 * method to deal with it properly anyway to show the null value. */
+		}
+		var type: UpdateType
+		var cause: Cause
+		var spec: Spec
+	}
+	
+	private func updateManifestAndWaitForSuccess(_ parameters: UpdateManifestData = UpdateManifestData(type: .sync, cause: UpdateManifestData.Cause(), spec: UpdateManifestData.Spec()), fluxURL: URL, handler: @escaping (_ result: Result<Void, Error>) -> Void) {
+		AF.request(fluxURL.appendingPathComponent("v9").appendingPathComponent("update-manifests"), method: .post, parameters: parameters, encoder: JSONParameterEncoder())
+			.responseDecodable(of: String.self, queue: self.loadQueue){ response in
+				switch response.result {
+					case .success(let id):    return self.waitForTaskSuccess(id, fluxURL: fluxURL, handler: handler)
+					case .failure(let error): return handler(.failure(error))
+				}
+			}
+	}
+	
+	private func waitForTaskSuccess(_ id: String, fluxURL: URL, handler: @escaping (_ result: Result<Void, Error>) -> Void) {
+		struct JobsResult : Decodable {
+			var Err: String
+			var StatusString: String
+			/* There actually are other properties. */
+		}
+		AF.request(fluxURL.appendingPathComponent("v6").appendingPathComponent("jobs"), parameters: ["id": id])
+			.responseDecodable(of: JobsResult.self, queue: self.loadQueue){ response in
+				switch response.result {
+					case .success(let result):
+						switch result.StatusString {
+							case "running", "queued": return self.waitForTaskSuccess(id, fluxURL: fluxURL, handler: handler)
+							case "succeeded":         return handler(.success(()))
+							default:                  return handler(.failure(SimpleError(message: "Unknown job status \"\(result.StatusString)\". Err = \"\(result.Err)\".")))
+						}
+						
+					case .failure(let error):
+						return handler(.failure(error))
+				}
+			}
+	}
+	
+	private func failDeployment(_ error: Error) {
+		DispatchQueue.main.sync{
+			self.deployStatus = .deployed(.failure(error))
+		}
+	}
 	
 }
